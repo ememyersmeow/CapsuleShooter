a7e2741ca49ee7fb212296112bf10a18413fc34f
ememyers <myersalp@gmail.com>
2024-02-22 00:09:39 +0300

    Ammo

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth = 100f;
+
+    
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
 
 public class Weapon : MonoBehaviour
 {
     public float damage = 20f;
-    public float fireRate = 10f;
     public float range = 200f;
     public ParticleSystem muzzleFlash;
     public AudioClip shotSFX;
     public AudioSource _audioSource;
     public Transform bulletSpawn;
 
+    public Image image;
+
+    [SerializeField] private int currentAmmo;
+    [SerializeField] private int maxAmmoInMag;
+    [SerializeField] private int totalAmmo;
+
+    [SerializeField] private float fireRate = 0.15f;
+
+    private float lastshot;
+
+    public TMP_Text ammoText;
+
     public Camera _cam;
     private float lastTimeFired = 0f;
+
+    public void Start()
+    {
+        ammoText.text = currentAmmo.ToString() + "/" + totalAmmo.ToString();
+    }
 
     void Update()
     {
@@ -23,10 +41,20 @@ public class Weapon : MonoBehaviour
         {
             Shoot();
         }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
     }
 
     void Shoot()
     {
+        if ((Time.time - lastshot) < fireRate || currentAmmo <= 0)
+            return;
+
+        lastshot = Time.time;
+        currentAmmo--;
         _audioSource.PlayOneShot(shotSFX);
         muzzleFlash.Play();
 
@@ -40,7 +68,10 @@ public class Weapon : MonoBehaviour
 
             if (hit.collider.tag == "Target")
             {
-                Destroy(hit.collider.gameObject);
+                hit.collider.GetComponent<EnemyHealth>().health -= damage;
+                image.enabled = true;
+                StartCoroutine(Hitmarker()); //start timer
+
             }
 
             if ((Time.time - lastTimeFired) > 1f / fireRate)
@@ -48,5 +79,28 @@ public class Weapon : MonoBehaviour
                 lastTimeFired = Time.time;
             }
         }
+
+        
+
+        ammoText.text = currentAmmo.ToString() + "/" + totalAmmo.ToString();
+    }
+
+    public void Reload()
+    {
+        if (currentAmmo >= maxAmmoInMag || totalAmmo <= 0)
+            return;
+        int needAmmo = maxAmmoInMag - currentAmmo;
+        int haveAmmo = Mathf.Min(needAmmo, totalAmmo);
+        if (haveAmmo == 0)
+            return;
+        totalAmmo -= haveAmmo;
+        currentAmmo += haveAmmo;
+        ammoText.text = currentAmmo.ToString() + "/" + totalAmmo.ToString();
+    }
+
+    public IEnumerator Hitmarker() // Timer
+    {
+        yield return new WaitForSeconds(0.07f); // after 0.07 sec delete
+        image.enabled = false;
     }
 }
